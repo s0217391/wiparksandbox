f8ac1e85eef612dba28c040ac118bb40d3b87ebd
Pieterjan Bartels <pieterjanbartels@gmail.com>
2017-11-27 15:48:38 +0100

    forgot self

--- flash_step/Wipark_flash/simple_pin.py
+++ flash_step/Wipark_flash/simple_pin.py
@@ -18,14 +18,14 @@ class Pin(object):
         assert type(pin_number) is int, self.pin_type_error_str
         self.pin_number = pin_number
         # Initialize pin
-        self.__write_to_file("{}/unexport".format(self.gpio_path), self.pin_number)
+        self.__write_to_file( "{}/unexport".format(self.gpio_path) , self.pin_number)
         self.__write_to_file("{}/export".format(self.gpio_path), self.pin_number)
 
-    def __write_to_file(filename, value):
+    def __write_to_file(self, filename, value):
         with open(filename, 'w') as f:
             f.write(value)
 
-    def get_pin_value():
+    def get_pin_value(self):
         value = 0
         with open("{}/gpio{}/value".format(self.gpio_path, self.pin_number), 'r') as f:
             contents = f.read()
@@ -33,10 +33,10 @@ class Pin(object):
         assert value == 0 or value == 1, self.returnvalue_value_error_str
         return value
 
-    def set_pin_direction(direction):
+    def set_pin_direction(self, direction):
         assert direction == self.OUTPUT_MODE or direction == self.INPUT_MODE, mode_value_error_str
         self.__write_to_file('{}/gpio{}/direction'.format(self.gpio_path, pin), direction)
 
-    def set_pin_value(value):
+    def set_pin_value(self, value):
         assert value == self.DISABLE or value == self.ENABLE, mode_value_error_str
         self.__write_to_file('{}/gpio{}/value'.format(self.gpio_path, pin), value)
